b8e1b9b6907f48f89808dfeddfe4c3283f8bdf2e
VirxEC <virx@virxcase.dev>
2022-04-07 11:01:28 -0400

    Update

--- develop.bat
+++ develop.bat
@@ -1,4 +1,4 @@
 @echo off
 set rlpy="%localappdata%\RLBotGUIX\Python37\python.exe"
 maturin build --release -i %rlpy%
-$rlpy -m pip install target/wheels/*.*.*-cp37-*.whl --force-reinstall
+%rlpy% -m pip install rl_ball_sym_pybinds --find-links=target\wheels --force-reinstall
