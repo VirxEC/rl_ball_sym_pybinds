8dca4238bf1167a09c52c4ac92b155cf5ae51c00
VirxEC <virx@virxcase.dev>
2022-12-03 18:44:07 -0500

    Don't spew out errors when framework prediction isn't ready

--- script.py
+++ script.py
@@ -19,17 +19,19 @@ class rl_ball_sym(BaseScript):
 
                 rlbs.tick(self.packet)
 
-                framework_prediction = self.get_ball_prediction_struct()
-                custom_prediction = rlbs.get_ball_prediction_struct()
-
                 self.renderer.begin_rendering()
-                self.renderer.draw_polyline_3d(tuple((framework_prediction.slices[i].physics.location.x, framework_prediction.slices[i].physics.location.y, framework_prediction.slices[i].physics.location.z) for i in range(0, framework_prediction.num_slices, 4)), self.renderer.green())
+
+                custom_prediction = rlbs.get_ball_prediction_struct()
                 self.renderer.draw_polyline_3d(tuple(custom_prediction.slices[i].location for i in range(0, custom_prediction.num_slices, 4)), self.renderer.red())
+
+                framework_prediction = self.get_ball_prediction_struct()
+                if framework_prediction is not None and framework_prediction.num_slices > 8:
+                    self.renderer.draw_polyline_3d(tuple((framework_prediction.slices[i].physics.location.x, framework_prediction.slices[i].physics.location.y, framework_prediction.slices[i].physics.location.z) for i in range(0, framework_prediction.num_slices, 4)), self.renderer.green())
+                
                 self.renderer.end_rendering()
             except Exception:
                 print_exc()
 
 
 if __name__ == "__main__":
-    rl_ball_sym = rl_ball_sym()
-    rl_ball_sym.main()
+    rl_ball_sym().main()
